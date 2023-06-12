a2b518a0a06a7ce1218bd40ed67a0d228114e37d
Y061217 <http1220@naver.com>
2023-06-13 00:00:40 +0900

    1

---
+++ src/main/java/com/devrun/Controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.devrun.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+	@GetMapping("/")
+	public ModelAndView home() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("index");
+		System.err.println("들어옴");
+		return mv;
+	}
+	
+}
---
+++ src/main/java/com/devrun/DevrunApplication.java
@@ -0,0 +1,13 @@
+package com.devrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevrunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevrunApplication.class, args);
+	}
+
+}
