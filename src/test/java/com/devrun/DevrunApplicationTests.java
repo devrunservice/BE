13206237cd672d700b5f90b18d101345d4aec644
Y061217 <http1220@naver.com>
2023-06-13 00:00:40 +0900

    1

---
+++ src/test/java/com/devrun/DevrunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevrunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
