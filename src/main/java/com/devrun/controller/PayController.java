70da0e9d64a9d6875ab248651698e6aa313b009f
SSunH <tjsgh1104@naver.com>
2023-06-25 18:45:30 +0900

    pay수정

--- src/main/java/com/devrun/controller/PayController.java
+++ src/main/java/com/devrun/controller/PayController.java
@@ -25,15 +25,8 @@ public class PayController {
 	@Value("${iamport.SECRET_Key}")
 	private String SECRET;
 	
-	private  IamportClient api;
-	
-	public PayController() {
-		
-		// 아임포트 나의 정보 값 넣기
-		this.api = new IamportClient
-				(KEY,SECRET);
-				
-	}
+	private  IamportClient api;	
+
 	@ResponseBody
 	// 아임포트 api 문서를 예시로 값 넣어주기
 	@RequestMapping(value="/verifyIamport/{imp_uid}")
@@ -43,7 +36,10 @@ public class PayController {
 			, HttpSession session
 			, @PathVariable(value= "imp_uid") String imp_uid) throws IamportResponseException, IOException
 	{	
+		// 아임포트 나의 정보 값 넣기
+			this.api = new IamportClient(KEY,SECRET);
 		//아임포트 서버에 imp_uid를 통해 값 받아와서 우리 서버랑 비교 후 같으면 결제 진행.
+
 			return api.paymentByImpUid(imp_uid);
 	}
 	
