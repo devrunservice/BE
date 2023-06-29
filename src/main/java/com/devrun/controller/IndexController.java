548cb013fce7a0c3c7e8e597dc441d59fee4dc09
Y061217 <http1220@naver.com>
2023-06-29 20:38:54 +0900

    swagger 기능 추가

--- src/main/java/com/devrun/controller/IndexController.java
+++ src/main/java/com/devrun/controller/IndexController.java
@@ -1,15 +1,19 @@
 package com.devrun.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
+@Api(tags = "Devrun")
 public class IndexController {
 
 	@GetMapping("/react1")
 	@ResponseBody
+	@ApiOperation("Get a greeting message")
 	public String home() {
 
 		return "react 환영합니다.";
