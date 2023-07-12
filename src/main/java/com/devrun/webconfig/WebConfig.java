cb1993c28b9967c03c545a584ee9e6c4424a3c1c
sung0763 <sung0763@naver.com>
2023-07-13 02:32:49 +0900

    Merge branch 'merge' of https://github.com/devrunservice/BE.git into merge

--- src/main/java/com/devrun/webconfig/WebConfig.java
+++ src/main/java/com/devrun/webconfig/WebConfig.java
@@ -12,7 +12,8 @@ public class WebConfig implements WebMvcConfigurer {
     			// 모든 경로에 대해 CORS 설정을 적용
         registry.addMapping("/**")
                 // 모든 출처에서 오는 요청을 허용
-				.allowedOrigins("http://localhost:3000" )
+//				.allowedOrigins("http://localhost:3000" )
+				.allowedOrigins("*")
                 // 모든 HTTP 메소드(GET, POST, PUT, DELETE 등)를 허용
         		.allowedMethods("*")
         		// 모든 헤더 허용
