22284656343c96630c82b4f429037302d730a7ea
SSunH <tjsgh1104@naver.com>
2023-07-13 23:35:52 +0900

    쿠폰 결제 테스트 x

--- src/main/java/com/devrun/controller/PayController.java
+++ src/main/java/com/devrun/controller/PayController.java
@@ -5,15 +5,21 @@ import java.util.Locale;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devrun.entity.CouponIssued;
+import com.devrun.entity.PaymentEntity;
+import com.devrun.repository.CouponIssuedRepository;
+import com.devrun.repository.PaymentRepository;
 import com.siot.IamportRestClient.IamportClient;
 import com.siot.IamportRestClient.exception.IamportResponseException;
 import com.siot.IamportRestClient.response.IamportResponse;
@@ -28,6 +34,12 @@ public class PayController {
 	private String SECRET;
 	
 	private  IamportClient api;	
+	
+	@Autowired
+	private CouponIssuedRepository CouponIssuedRepository;
+	
+	@Autowired
+	private PaymentRepository PaymentRepository;
 
 	@ResponseBody
 	// 아임포트 api 문서를 예시로 값 넣어주기
@@ -36,14 +48,28 @@ public class PayController {
 	        Model model,
 	        Locale locale,
 	        HttpSession session,
-	        @PathVariable(value = "imp_uid") String imp_uid) {
+	        @PathVariable(value = "imp_uid") String imp_uid,
+	        @RequestParam(value = "couponCode", required = false) String couponCode) {
 	    try {
 			// 아임포트 나의 정보 값 넣기
 	        this.api = new IamportClient(KEY, SECRET);
 	        
 			//아임포트 서버에 imp_uid를 통해 값 받아와서 우리 서버랑 비교 후 같으면 결제 진행.
 	        IamportResponse<Payment> response = api.paymentByImpUid(imp_uid);
+	        
+	        //쿠폰 적용시
+	        if(couponCode !=null) {
+	            CouponIssued couponIssued = CouponIssuedRepository.findByDiscountrate(couponCode);
+		        PaymentEntity paymentEntity = PaymentRepository.findByPaidAmount(imp_uid);
+
+	            int orgAmount =paymentEntity.getPaid_amount();	            		
+	            int discountAmount =couponIssued.getDiscountrate();
+	            int finallyAmount = orgAmount - discountAmount;
+	            paymentEntity.setPaid_amount(finallyAmount);
+	        }
+	        
 	        return ResponseEntity.ok(response);
+
 	    } catch (IamportResponseException e) {
 	        // 아임포트 API 호출 중에 예외가 발생한 경우
 	        String errorMessage = "아임포트 API 오류: " + e.getMessage();
