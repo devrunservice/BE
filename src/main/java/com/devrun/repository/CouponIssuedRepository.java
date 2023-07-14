f3914b4f74afa864e294409560e370c986497a66
sung0763 <sung0763@naver.com>
2023-07-14 13:36:06 +0900

    Revert "Revert "쿠폰 결제 테스트 x""
    This reverts commit 33105de62a72ee7c5fbe144afbf12d6504f781a9.

--- src/main/java/com/devrun/repository/CouponIssuedRepository.java
+++ src/main/java/com/devrun/repository/CouponIssuedRepository.java
@@ -6,4 +6,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CouponIssuedRepository extends JpaRepository<CouponIssued, Long> {
+
+	CouponIssued findByDiscountrate(String couponCode);
+
+	
+
 }
