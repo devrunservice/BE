0df744213ba07c03581214d6c6870ba4df232a38
SSunH <tjsgh1104@naver.com>
2023-07-31 21:37:31 +0900

    결제 db 저장 배열로 받기

--- src/main/java/com/devrun/controller/PaymentController.java
+++ src/main/java/com/devrun/controller/PaymentController.java
@@ -3,6 +3,7 @@ package com.devrun.controller;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.devrun.dto.PaymentDTO;
 import com.devrun.entity.PaymentEntity;
 import com.devrun.repository.PaymentRepository;
 import com.devrun.service.PaymentService;
@@ -27,20 +29,44 @@ public class PaymentController {
 	private PaymentRepository paymentRepository;
 
 	// 결제 정보 db에 저장
-	@PostMapping("/savePaymentInfo")
-	public ResponseEntity<String> savePaymentInfo(@RequestBody PaymentEntity paymentEntity) {
-		try {
+		@PostMapping("/savePaymentInfo")
+		public ResponseEntity<String> savePaymentInfo(@RequestBody List<PaymentDTO> paymentDTOList) {
+			
 			LocalDateTime dateTime = LocalDateTime.now();
 			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd a hh:mm:ss", new Locale("ko"));
 			String paymentDate = dateTime.format(formatter);
-			paymentEntity.setPaymentDate(paymentDate);
-			paymentEntity.setStatus("0");
-			paymentService.savePaymentInfo(paymentEntity);
-			return ResponseEntity.ok("결제 정보가 성공적으로 저장되었습니다.");
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("결제 정보 저장에 실패했습니다.");
+			System.err.println(paymentDTOList);
+			
+			try {			
+				List<PaymentEntity> paymentList = new ArrayList<>();
+		        for (PaymentDTO paymentDTO : paymentDTOList) {
+		        PaymentEntity paymentEntity = new PaymentEntity();
+		        paymentEntity.setName(paymentDTO.getName());
+		        paymentEntity.setPaid_amount(paymentDTO.getPaid_amount());
+	            paymentEntity.setBuyer_email(paymentDTO.getBuyer_email());
+	            paymentEntity.setBuyer_name(paymentDTO.getBuyer_name());
+	            paymentEntity.setImp_uid(paymentDTO.getImp_uid());
+	            paymentEntity.setMerchant_uid(paymentDTO.getMerchant_uid());
+	            paymentEntity.setReceipt_url(paymentDTO.getReceipt_url());
+	            paymentEntity.setBuyer_tel(paymentDTO.getBuyer_tel());
+
+				paymentEntity.setPaymentDate(paymentDate);
+				paymentEntity.setStatus("0");
+				
+				System.out.println(paymentEntity);					
+	            paymentList.add(paymentEntity);
+
+				
+		       }
+		        System.err.println(paymentList);
+				paymentService.savePaymentInfo(paymentList);
+
+				
+				return ResponseEntity.ok("결제 정보가 성공적으로 저장되었습니다.");
+			} catch (Exception e) {
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("결제 정보 저장에 실패했습니다.");
+			}
 		}
-	}
 	
 	// 결제 정보
 	
--- src/main/java/com/devrun/repository/PaymentRepository.java
+++ src/main/java/com/devrun/repository/PaymentRepository.java
@@ -1,9 +1,11 @@
 package com.devrun.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import com.devrun.entity.PaymentEntity;
 
 @Repository
@@ -15,6 +17,8 @@ public interface PaymentRepository extends JpaRepository<PaymentEntity, Long> {
 		 
 		 @Query("SELECT p FROM PaymentEntity p WHERE p.paid_amount = :paid_amount")
 		 PaymentEntity findByPaidAmount(@Param("paid_amount") String imp_uid);
+
+
 
 	
 
