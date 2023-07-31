6fede680d5d2b0040d016df8f635d5e9ab51ae6b
Y061217 <http1220@naver.com>
2023-07-31 21:04:07 +0900

    장바구니브랜치 머지 브랜치 통합 후 임시저장 파일 불러오기# Please enter the commit message for your changes. Lines starting

--- src/main/java/com/devrun/config/SecurityConfig.java
+++ src/main/java/com/devrun/config/SecurityConfig.java
@@ -30,9 +30,11 @@ public class SecurityConfig {
             .authorizeRequests(authorize -> authorize
                     .antMatchers(HttpMethod.OPTIONS, "/**/*").permitAll()
                     .antMatchers("/tmi").authenticated()// 인증이 필요한 /tmi 엔드포인트
+                    .antMatchers("/api/*").authenticated()
                     .antMatchers("/token/refresh").permitAll()
                     .antMatchers("/logout").permitAll()
                 .anyRequest().permitAll())
+
             .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
             .logout().disable()
             .build();
--- src/main/java/com/devrun/controller/CartController.java
+++ src/main/java/com/devrun/controller/CartController.java
@@ -4,11 +4,13 @@ import com.devrun.entity.Cart;
 import com.devrun.entity.Lecture;
 import com.devrun.entity.MemberEntity;
 import com.devrun.repository.CartRepo;
-import com.devrun.service.SignupService;
+import com.devrun.service.MemberService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,7 +24,7 @@ public class CartController {
     private CartRepo cartRepo;
 
     @Autowired
-    private SignupService signupService;
+    private MemberService memberService;
 
     @GetMapping("/api/cart")
     @ResponseBody
@@ -31,16 +33,24 @@ public class CartController {
             , value = "강의 일련번호"
     )
     public String putCart(@RequestParam(name = "lno") int lno) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+
+
+
         String msg;
-        String userid = "bbb222";
-        MemberEntity memberEntity = signupService.findById(userid);
+        String userid = authentication.getName();
+        System.out.println("유저 이름은 : " + userid);
+
+        MemberEntity memberEntity = memberService.findById(userid);
         if(memberEntity.getId() != null){
             Cart cart = new Cart();
             Lecture lecture = new Lecture();
             lecture.setLno(1);
             cart.setLecture(lecture);
             cart.setMemberEntity(memberEntity);
-            cartRepo.save(cart);
+            //cartRepo.save(cart);
             msg = "저장 성공";
 
         } else {
@@ -54,8 +64,11 @@ public class CartController {
     @ResponseBody
     public String deleteInCart(@RequestParam(name = "lecturetitle") String lecturetitle){
 
-        String userid = "bbb222";
-        cartRepo.deleteInCart(userid , lecturetitle);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userid = authentication.getName();
+        System.out.println("유저 이름은 : " + userid);
+
+        //cartRepo.deleteInCart(userid , lecturetitle);
 
         return "삭제 성공";
     }
