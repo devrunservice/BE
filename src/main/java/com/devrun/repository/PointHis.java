f8bf6985ba1d9ca1e9eb19205c41b9d9b9f3beef
SSunH <tjsgh1104@naver.com>
2023-09-07 02:52:29 +0900

    포인트 히스토리 구현 , 구매 페이지 수정중

---
+++ src/main/java/com/devrun/repository/PointHis.java
@@ -0,0 +1,10 @@
+package com.devrun.repository;
+
+public interface PointHis {
+	
+	String getExplanation();
+	int getPointupdown();
+	String getUpdatetime();	
+	int getPointno();
+
+}
---
+++ src/main/java/com/devrun/repository/PointHistoryRepository.java
@@ -0,0 +1,29 @@
+package com.devrun.repository;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import com.devrun.entity.PointHistoryEntity;
+
+@Repository
+public interface PointHistoryRepository extends JpaRepository<PointHistoryEntity, Long> {
+
+	@Query(value = "SELECT p.explanation AS explanation, p.pointupdown AS pointupdown, "
+	        + "p.updatetime AS updatetime, "
+	        + "ROW_NUMBER() OVER(ORDER BY p.updatetime DESC) AS pointno "
+	        + "FROM pointhistory p "
+	        + "WHERE p.user_no = :usrno", nativeQuery = true)
+	Page<PointHis> findAllbyPointHistoryEntity(@Param("usrno") int usrno, PageRequest pageRequest);
+
+
+
+
+
+   
+
+
+}
