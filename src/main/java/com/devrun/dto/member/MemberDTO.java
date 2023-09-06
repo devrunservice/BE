812311c727115f5b2095c9dbf06af6e4db9047a8
sung0763 <sung0763@naver.com>
2023-09-06 14:19:42 +0900

    memberDTO 기본생성자 추가

--- src/main/java/com/devrun/dto/member/MemberDTO.java
+++ src/main/java/com/devrun/dto/member/MemberDTO.java
@@ -5,11 +5,13 @@ import java.util.Date;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class MemberDTO {
 	
 	private String name, email, id, password, phonenumber;
