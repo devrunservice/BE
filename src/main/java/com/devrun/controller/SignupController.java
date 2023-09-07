74229bab63f72b404f319fee0f04f04bbf137ec1
sung0763 <sung0763@naver.com>
2023-09-07 20:02:41 +0900

    이메일 인증 테스트

--- src/main/java/com/devrun/controller/SignupController.java
+++ src/main/java/com/devrun/controller/SignupController.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -400,6 +401,7 @@ public class SignupController {
 	// 회원가입 인증 확인
 	@ResponseBody
 	@Transactional
+	@CrossOrigin(origins = "https://mail.naver.com")
 	@PostMapping("/verify/signupEmail")
 	public ResponseEntity<?> signupOk(@RequestParam("id") String id
 										, @RequestParam("key") String key){
