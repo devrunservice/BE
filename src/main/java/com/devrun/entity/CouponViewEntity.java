7cf9d86e42b45bfeaea7ccb766df41d7827ba48d
SSunH <tjsgh1104@naver.com>
2023-09-10 02:54:47 +0900

    뷰 페이지 ENUM 추가

--- src/main/java/com/devrun/entity/CouponViewEntity.java
+++ src/main/java/com/devrun/entity/CouponViewEntity.java
@@ -1,12 +1,12 @@
 package com.devrun.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Immutable;
-
-import com.devrun.entity.Couponregicode.couponstate;
 
 import lombok.Data;
 
@@ -23,7 +23,7 @@ public class CouponViewEntity {
 
     private String couponcode;
 
-    private int userno;
+    private Integer userno;
 
     private int discountrate;
 
@@ -34,7 +34,15 @@ public class CouponViewEntity {
     private int quantity;
 
     private String target;
+   
+    @Enumerated(EnumType.STRING)
+    private couponstate state; // 이 부분 추가
 
-    private couponstate state;
+    public enum couponstate {
+        ACTIVE, // 사용 가능 (생성 시 기본값)
+        REMOVED, // 사용 정지 처리로 인한 사용 불가
+        EXPIRY, // 기간 만료로 인한 사용 불가
 
+  
+    }
 }
