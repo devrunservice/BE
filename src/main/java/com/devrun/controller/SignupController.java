5dafde0c7c9ae33eb5683292eac2dcb5667e8f7f
sung0763 <sung0763@naver.com>
2023-09-14 22:35:22 +0900

    에러메시지 출력

--- src/main/java/com/devrun/controller/SignupController.java
+++ src/main/java/com/devrun/controller/SignupController.java
@@ -248,7 +248,7 @@ public class SignupController {
 				return sendEmail(toEmail, id);
 			} catch (Exception e) {
 				e.printStackTrace();
-				return ResponseEntity.status(500).body("Decryption failed");
+				return ResponseEntity.status(500).body("Decryption failed" + e);
 			}
 		} else if (data == null && email != null && id != null) {
 			return sendEmail(email, id);
