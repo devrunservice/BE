34e2d60fb137f428aee6b0cefdc9c3f7cbe6f274
sung0763 <sung0763@naver.com>
2023-09-14 23:01:46 +0900

    AESUtil에 URL-safe 적용 #7

--- src/main/java/com/devrun/util/AESUtil.java
+++ src/main/java/com/devrun/util/AESUtil.java
@@ -1,6 +1,5 @@
 package com.devrun.util;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 import javax.crypto.Cipher;
@@ -22,18 +21,18 @@ public class AESUtil {
     private static final String ALGORITHM = "AES";
 
     public static String encrypt(String data) throws Exception {
-        SecretKeySpec skeySpec = new SecretKeySpec(SECRET_KEY.getBytes(StandardCharsets.UTF_8), ALGORITHM);
+        SecretKeySpec skeySpec = new SecretKeySpec(SECRET_KEY.getBytes(), ALGORITHM);
         Cipher cipher = Cipher.getInstance(ALGORITHM);
         cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
-        byte[] encrypted = cipher.doFinal(data.getBytes(StandardCharsets.UTF_8));
-        return Base64.getEncoder().encodeToString(encrypted);
+        byte[] encrypted = cipher.doFinal(data.getBytes());
+        return Base64.getUrlEncoder().encodeToString(encrypted);
     }
 
     public static String decrypt(String encryptedData) throws Exception {
-        SecretKeySpec skeySpec = new SecretKeySpec(SECRET_KEY.getBytes(StandardCharsets.UTF_8), ALGORITHM);
+        SecretKeySpec skeySpec = new SecretKeySpec(SECRET_KEY.getBytes(), ALGORITHM);
         Cipher cipher = Cipher.getInstance(ALGORITHM);
         cipher.init(Cipher.DECRYPT_MODE, skeySpec);
-        byte[] original = cipher.doFinal(Base64.getDecoder().decode(encryptedData));
-        return new String(original, StandardCharsets.UTF_8);
+        byte[] original = cipher.doFinal(Base64.getUrlDecoder().decode(encryptedData));
+        return new String(original);
     }
 }
