efcb8be19483575fb2045e9fbb514d74a534ef31
sung0763 <sung0763@naver.com>
2023-09-15 21:09:24 +0900

    더이상 /notice/write가 작성 뿐만 아니라 수정까지 하지 않도록 수정 #7
    스웨거 추가

--- src/main/java/com/devrun/controller/NoticeController.java
+++ src/main/java/com/devrun/controller/NoticeController.java
@@ -22,6 +22,12 @@ import com.devrun.entity.MemberEntity;
 import com.devrun.entity.Notice;
 import com.devrun.service.NoticeService;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @Controller
 public class NoticeController {
 
@@ -32,40 +38,34 @@ public class NoticeController {
 	@Transactional
 	@ResponseBody
 	@PostMapping("/notice/write")
+	@ApiOperation(value = "공지사항 작성/수정", notes = "공지사항을 작성하거나 수정합니다.")
+	@ApiImplicitParams({
+		@ApiImplicitParam(name = "title", value = "공지사항 제목", required = true, paramType = "body", dataTypeClass = String.class),
+		@ApiImplicitParam(name = "content", value = "공지사항 내용", required = true, paramType = "body", dataTypeClass = String.class)
+	})
+	@ApiResponses(value = {
+	    @ApiResponse(code = 200, message = "공지사항 작성/수정 성공"),
+	    @ApiResponse(code = 400, message = "잘못된 입력값 또는 MemberEntity를 찾을 수 없음"),
+	    @ApiResponse(code = 500, message = "내부 서버 오류")})
 	public ResponseEntity<?> notice(@RequestBody NoticeDTO noticeDTO) {
 		try {
 		    System.out.println("noticeDTO : " + noticeDTO);
-		    String title = 
-//		    		textChange.changeText(
-		    				noticeDTO.getTitle()
-//		    				)
-		    		;
-		    String content = 
-//		    		textChange.changeText(
-		    				noticeDTO.getContent()
-//		    				)
-		    		;
-		    // 기존 Notice 찾기, 없으면 새로 생성
-		    Notice notice = noticeService.findByNoticeNo(noticeDTO.getNoticeNo());
-		    System.out.println("notice : " + notice);
-	
-		    if (notice == null) {
-		        notice = new Notice();
-		        notice.setNoticeNo(noticeDTO.getNoticeNo());
-		        
-		        // 관련된 MemberEntity 찾기
-		        MemberEntity memberEntity = noticeService.findById(noticeDTO.getId());
-		        
-		        // MemberEntity가 null인지 확인
-		        if (memberEntity == null) {
-		            return ResponseEntity.status(400).body("MemberEntity not found");
-		        }
-		        
-		        // Notice 엔터티에 MemberEntity 설정
-		        notice.setMemberEntity(memberEntity);
-		    }
-	
-		    System.out.println("notice2 : " + notice);
+		    String title = noticeDTO.getTitle();
+		    String content = noticeDTO.getContent();
+		    
+		 // 항상 새로운 Notice 객체를 생성
+	        Notice notice = new Notice();
+	        
+	        // 관련된 MemberEntity 찾기
+	        MemberEntity memberEntity = noticeService.findById(noticeDTO.getId());
+	        
+	        // MemberEntity가 null인지 확인
+	        if (memberEntity == null) {
+	            return ResponseEntity.status(400).body("MemberEntity not found");
+	        }
+	        
+	        // Notice 엔터티에 MemberEntity 설정
+	        notice.setMemberEntity(memberEntity);
 	
 		    // 필요한 필드 업데이트
 		    notice.setTitle(title);
@@ -89,7 +89,14 @@ public class NoticeController {
 //    }
 	
 	// 공지사항 페이징
+	@ResponseBody
 	@GetMapping("/notice/{pageNumber}")
+	@ApiOperation(value = "공지사항 페이징", notes = "페이지 번호에 따른 공지사항 목록을 반환합니다.")
+	@ApiImplicitParam(name = "pageNumber", value = "페이지 번호", required = true, paramType = "path", dataTypeClass = Integer.class)
+	@ApiResponses(value = {
+	    @ApiResponse(code = 200, message = "공지사항 목록을 성공적으로 반환했습니다."),
+	    @ApiResponse(code = 404, message = "공지사항이 없습니다.")
+	})
 	public ResponseEntity<Page<NoticeDTO>> getAllNotices(@PathVariable int pageNumber) {
 	    // 'createdDate'를 기준으로 내림차순 정렬
 	    Pageable pageable = PageRequest.of(pageNumber - 1, 10, Sort.by(
@@ -108,6 +115,13 @@ public class NoticeController {
 	// 공지사항 읽기
 	@ResponseBody
 	@GetMapping("/notice/detail/{noticeNo}")
+	@ApiOperation(value = "공지사항 읽기", notes = "공지사항 번호에 따른 상세 정보를 반환합니다.")
+	@ApiImplicitParam(name = "noticeNo", value = "공지사항 번호", required = true, paramType = "path", dataTypeClass = Integer.class)
+	@ApiResponses(value = {
+	    @ApiResponse(code = 200, message = "공지사항 상세 정보를 성공적으로 반환했습니다."),
+	    @ApiResponse(code = 404, message = "공지사항을 찾을 수 없습니다."),
+	    @ApiResponse(code = 500, message = "내부 서버 오류")
+	})
 	public ResponseEntity<?> getNotice(@PathVariable int noticeNo) {
 	    try {
 	        Notice notice = noticeService.getNoticeByNoticeNo(noticeNo);
@@ -127,7 +141,19 @@ public class NoticeController {
 	}
 	
 	// 공지사항 수정
+	@ResponseBody
 	@PutMapping("/notice/edit/{noticeNo}")
+	@ApiOperation(value = "공지사항 수정", notes = "공지사항의 제목과 내용을 수정합니다.")
+	@ApiImplicitParams({
+	    @ApiImplicitParam(name = "noticeNo", value = "수정할 공지사항 번호", required = true, paramType = "path", dataTypeClass = Integer.class),
+	    @ApiImplicitParam(name = "title", value = "수정할 공지사항 제목", required = true, paramType = "body", dataTypeClass = String.class),
+	    @ApiImplicitParam(name = "content", value = "수정할 공지사항 내용", required = true, paramType = "body", dataTypeClass = String.class)
+	})
+	@ApiResponses(value = {
+	    @ApiResponse(code = 200, message = "공지사항을 성공적으로 수정했습니다."),
+	    @ApiResponse(code = 400, message = "잘못된 인수입니다."),
+	    @ApiResponse(code = 500, message = "내부 서버 오류")
+	})
 	public ResponseEntity<?> updateNotice(@PathVariable int noticeNo, @RequestBody NoticeDTO noticeDTO) {
 	    try {
 	        String newTitle = noticeDTO.getTitle();
