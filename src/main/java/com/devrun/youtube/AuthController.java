ed4783b017e0431c90b975949b150c1d11958234
gotka153 <gotka153@naver.com>
2023-09-18 23:16:05 +0900

    임포트수정

--- src/main/java/com/devrun/youtube/AuthController.java
+++ src/main/java/com/devrun/youtube/AuthController.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,7 +24,6 @@ import com.google.api.client.json.jackson2.JacksonFactory;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api/auth")
