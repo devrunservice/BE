773dfd19605e7ad0f6184e543046cbd9b67748b6
http1220 <http1220@naver.com>
2023-11-01 01:25:31 +0900

    단순 코드 정리

--- src/main/java/com/devrun/dto/SectionInfo.java
+++ src/main/java/com/devrun/dto/SectionInfo.java
@@ -1,11 +1,7 @@
 package com.devrun.dto;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import com.devrun.entity.MyLecture;
-import com.devrun.entity.MyLectureProgress;
-import com.devrun.youtube.Lecture;
 import com.devrun.youtube.LectureSection;
 
 import lombok.Data;
@@ -13,13 +9,12 @@ import lombok.Data;
 @Data
 public class SectionInfo {
 	/*
-	 * 섹션 정보
-	 * 섹션 ID, 섹션 제목, 섹션 번호
+	 * 섹션 정보 섹션 ID, 섹션 제목, 섹션 번호
 	 */
 	private final Long sectionId;
 	private final int sectionNumber;
 	private final String sectionTitle;
-	
+
 	private List<VideoInfo> videoInfo;
 
 	public SectionInfo(LectureSection ls) {
@@ -28,5 +23,5 @@ public class SectionInfo {
 		this.sectionNumber = ls.getSectionNumber();
 		this.sectionTitle = ls.getSectionTitle();
 	}
-	
+
 }
