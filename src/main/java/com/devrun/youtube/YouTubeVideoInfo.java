adbdb34dbad5300f4099ee0a322cde0c6d67425d
http1220 <http1220@naver.com>
2023-11-16 22:05:03 +0900

    Test : 강의 등록 영상 메타데이터 조회 기능 테스트 3

--- src/main/java/com/devrun/youtube/YouTubeVideoInfo.java
+++ src/main/java/com/devrun/youtube/YouTubeVideoInfo.java
@@ -15,8 +15,10 @@ import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.Video;
@@ -26,9 +28,13 @@ import com.google.api.services.youtube.model.VideoListResponse;
 public class YouTubeVideoInfo {
 	private static final String CLIENT_SECRETS = "client_secret.json";
 	private static final Collection<String> SCOPES = Arrays.asList("https://www.googleapis.com/auth/youtube.readonly");
-
 	private static final String APPLICATION_NAME = "API code samples";
-	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+	private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+	private HttpTransport httpTransport;
+
+	public YouTubeVideoInfo() throws Exception {
+		httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+	}
 
 	/**
 	 * Build and return an authorized API client service.
@@ -51,16 +57,22 @@ public class YouTubeVideoInfo {
 	 */
 	public List<VideoDto> getVideoInfo(List<VideoDto> uploadedVideos, HttpServletResponse httpServletResponse,
 			String accessToken) throws GeneralSecurityException, IOException, GoogleJsonResponseException {
-
+		System.out.println("---------------------------------------메타 데이터 받아오기 시작----------------------------------");
 		String videoIdlist = "";
 
 		for (VideoDto v : uploadedVideos) {
 			videoIdlist += v.getVideoId() + ",";
 		}
+		System.out.println("videoIdlist : "+ videoIdlist);
+		
+		Credential credential = new GoogleCredential().setAccessToken(accessToken);
 
-		YouTube youtubeService = getService(accessToken);
+		// YouTube 객체 초기화
+		YouTube youtube = new YouTube.Builder(httpTransport, JSON_FACTORY, credential)
+				.setApplicationName(APPLICATION_NAME).build();
+
 		// Define and execute the API request
-		YouTube.Videos.List request = youtubeService.videos().list("snippet,contentDetails,statistics");
+		YouTube.Videos.List request = youtube.videos().list("snippet,contentDetails,statistics");
 		VideoListResponse response = request.setId(videoIdlist).execute();
 
 		System.out.println(response);
