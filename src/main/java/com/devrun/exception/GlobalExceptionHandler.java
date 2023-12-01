98e2ce59dd244a2637bf80642e323705109f516f
sung0763 <sung0763@naver.com>
2023-12-02 02:07:09 +0900

    ㅅㄷㄴㅅ

--- src/main/java/com/devrun/exception/GlobalExceptionHandler.java
+++ src/main/java/com/devrun/exception/GlobalExceptionHandler.java
@@ -36,34 +36,34 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(RestApiException.class)
-    public ResponseEntity<Object> handleCustomException(RestApiException e) {
-        ErrorCode errorCode = e.getErrorCode();
-        return handleExceptionInternal(errorCode);
-    }
-    
-    private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode) {
-        return ResponseEntity.status(errorCode.getHttpStatus())
-                .body(makeErrorResponse(errorCode));
-    }
-    
-    private ErrorResponse makeErrorResponse(ErrorCode errorCode) {
-        return ErrorResponse.builder()
-                .code(errorCode.name())
-                .message(errorCode.getMessage())
-                .build();
-    }
-    
-    @ExceptionHandler(NullPointerException.class)
-    public ResponseEntity<Object> nullex(final NullPointerException e) {
-        Map<String, String> responeMap = new HashMap<>();
-        e.printStackTrace();
-        responeMap.put("Common Message", "Can't Find Data, Check Your Request. -By DevRun");
-        responeMap.put("Error_Locate", e.getStackTrace()[0].getClassName() + ":"+e.getStackTrace()[0].getLineNumber());
-        responeMap.put("Detail Error" , e.getMessage());
-        
-        return ResponseEntity.badRequest().body(responeMap);
-    }
+//    @ExceptionHandler(RestApiException.class)
+//    public ResponseEntity<Object> handleCustomException(RestApiException e) {
+//        ErrorCode errorCode = e.getErrorCode();
+//        return handleExceptionInternal(errorCode);
+//    }
+//    
+//    private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode) {
+//        return ResponseEntity.status(errorCode.getHttpStatus())
+//                .body(makeErrorResponse(errorCode));
+//    }
+//    
+//    private ErrorResponse makeErrorResponse(ErrorCode errorCode) {
+//        return ErrorResponse.builder()
+//                .code(errorCode.name())
+//                .message(errorCode.getMessage())
+//                .build();
+//    }
+//    
+//    @ExceptionHandler(NullPointerException.class)
+//    public ResponseEntity<Object> nullex(final NullPointerException e) {
+//        Map<String, String> responeMap = new HashMap<>();
+//        e.printStackTrace();
+//        responeMap.put("Common Message", "Can't Find Data, Check Your Request. -By DevRun");
+//        responeMap.put("Error_Locate", e.getStackTrace()[0].getClassName() + ":"+e.getStackTrace()[0].getLineNumber());
+//        responeMap.put("Detail Error" , e.getMessage());
+//        
+//        return ResponseEntity.badRequest().body(responeMap);
+//    }
     //not null 처리한 필드에 null이 들어가는 경우에도 ConstraintViolationException이 발생
     //컬럼의 제약 조건 위배 시 발생하는 예외
 //    @ExceptionHandler(value = ConstraintViolationException.class)
@@ -94,18 +94,18 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 
 //    @ExceptionHandler(value = HttpMessageNotReadableException.class)
-    @Override
-    protected ResponseEntity<Object> handleHttpMessageNotReadable(
-            HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-            Map<String ,String> responemap = new HashMap<>();
-            responemap.put("message" , "Can't Read HTTP Request, Check Your Request data type. ex json, xml, text, Number, Array, Object etc. -By DevRun");
-
-        return handleExceptionInternal(ex, responemap, headers, status, request);
-    }
-
-    protected String getPopertyName(String propertyPath) {
-        return propertyPath.substring(propertyPath.lastIndexOf('.') + 1); // 전체 속성 경로에서 속성 이름만 가져온다.
-    }
+//    @Override
+//    protected ResponseEntity<Object> handleHttpMessageNotReadable(
+//            HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+//            Map<String ,String> responemap = new HashMap<>();
+//            responemap.put("message" , "Can't Read HTTP Request, Check Your Request data type. ex json, xml, text, Number, Array, Object etc. -By DevRun");
+//
+//        return handleExceptionInternal(ex, responemap, headers, status, request);
+//    }
+//
+//    protected String getPopertyName(String propertyPath) {
+//        return propertyPath.substring(propertyPath.lastIndexOf('.') + 1); // 전체 속성 경로에서 속성 이름만 가져온다.
+//    }
 
 
 
