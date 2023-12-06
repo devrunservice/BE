67dc26a445704ce58db5fc7f68b3adae9b2d1faf
http1220 <http1220@naver.com>
2023-12-06 21:56:30 +0900

    오타 수정

--- src/main/java/com/devrun/dto/S3PathEnum.java
+++ src/main/java/com/devrun/dto/S3PathEnum.java
@@ -2,8 +2,8 @@ package com.devrun.dto;
 
 public enum S3PathEnum {
     profile,
-    lectuer_thumbnail,
-    lectuer_introduce,
+    lecture_thumbnail,
+    lecture_introduce,
     notice,
     lecture_note,
     lecture_qa,
